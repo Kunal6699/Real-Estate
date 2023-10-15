0e079b1c416005dd13fd11ca1a50b575f56aab2b
Kunal Kumar <kumarkunalkarn@gmail.com>
2023-10-15 14:16:57 +0530

    Making changes to  define options for handling a delete request in an API using Fastify

--- backend-fastify/src/routes/enquiries/options/delete-enquiry.js
+++ backend-fastify/src/routes/enquiries/options/delete-enquiry.js
@@ -1,6 +1,12 @@
 import { enquiryProperties } from "./schema.js";
 import { responseSuccess, responseError } from '../../../utils/schema/response.js';
 
+/**
+ * Options for handling a delete request for an Enquiry.
+ * @param {Object} fastify - The Fastify instance.
+ * @param {Function} handler - The request handler.
+ * @returns {Object} - Fastify route options.
+ */
 export const deleteEnquiryOpts = (fastify, handler) => ({
   preValidation: [fastify.authenticate],
   schema: {
@@ -9,10 +15,13 @@ export const deleteEnquiryOpts = (fastify, handler) => ({
         message: "Enquiry deleted!",
         data: enquiryProperties
       }),
-      400: responseError(),
+      400: responseError({
+        status: 400,
+        message: "Bad Request. Invalid input or parameters."
+      }),
       404: responseError({
         status: 404,
-        message: "Can't find Enquiry."
+        message: "Enquiry not found."
       })
     },
   },
